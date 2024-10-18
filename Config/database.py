38018b0e114c5b81b167143a2ac821b5ab5644da
zedhog2002 <46309525+zedhog2002@users.noreply.github.com>
2024-10-19 02:32:26 +0530

    collecion names fixed

--- Config/database.py
+++ Config/database.py
@@ -19,10 +19,10 @@ try:
     db = client.dyscalculia
     print("connection done")
     # Define the collections you will be working with
-    user_registration_collection = db["UserRegistrations"]
-    user_profile_collection = db["UserProfiles"]
+    user_registration_collection = db["Users_Reg"]
+    user_profile_collection = db["User_Profile"]
     quiz_collection = db["Quizzes"]
-    questions_collection = db["QuizQuestions"]
+    #questions_collection = db["QuizQuestions"]
     predicted_values_collection = db["PredictedResults"]
 except Exception as e:
     print(f"error connecting: {e}")
