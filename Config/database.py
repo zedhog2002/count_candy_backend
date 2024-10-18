d770cbb4724af608249f1ea1c869e91fdf89d129
zedhog2002 <46309525+zedhog2002@users.noreply.github.com>
2024-10-19 02:02:53 +0530

    Update database.py

--- Config/database.py
+++ Config/database.py
@@ -9,18 +9,21 @@ db_name = os.getenv("MONGO_DB_NAME")
 mongo_url = os.getenv("MONGO_URL")
 
 # Create the MongoDB client using environment variables
-client = MongoClient(
-    f"mongodb+srv://{username}:{password}@{mongo_url}/?retryWrites=true&w=majority&appName={db_name}",
-    tls=True,  # Use TLS for encryption
-    tlsAllowInvalidCertificates=True  # Optionally allow invalid certs
-)
-
-# Access the specified database
-db = client.dyscalculia
-
-# Define the collections you will be working with
-user_registration_collection = db["UserRegistrations"]
-user_profile_collection = db["UserProfiles"]
-quiz_collection = db["Quizzes"]
-questions_collection = db["QuizQuestions"]
-predicted_values_collection = db["PredictedResults"]
+try:
+    client = MongoClient(
+        f"mongodb+srv://{username}:{password}@{mongo_url}/?retryWrites=true&w=majority&appName={db_name}",
+        tls=True,  # Use TLS for encryption
+        tlsAllowInvalidCertificates=True  # Optionally allow invalid certs
+    )
+    
+    # Access the specified database
+    db = client.dyscalculia
+    print("connection done")
+    # Define the collections you will be working with
+    user_registration_collection = db["UserRegistrations"]
+    user_profile_collection = db["UserProfiles"]
+    quiz_collection = db["Quizzes"]
+    questions_collection = db["QuizQuestions"]
+    predicted_values_collection = db["PredictedResults"]
+except Exception as e:
+    print(f"error connecting: {e}")
