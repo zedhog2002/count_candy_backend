87fa99558a129c806987302c0447c6809ec1c8aa
zedhog2002 <46309525+zedhog2002@users.noreply.github.com>
2024-10-19 01:14:46 +0530

    Update database.py

--- Config/database.py
+++ Config/database.py
@@ -1,24 +1,26 @@
+import os
 from pymongo import MongoClient
 from urllib.parse import quote_plus
 
-username = quote_plus("krssn")
-password = quote_plus("krssn@capstone")
+# Read the MongoDB credentials from environment variables
+username = quote_plus(os.getenv("MONGO_USERNAME"))
+password = quote_plus(os.getenv("MONGO_PASSWORD"))
+db_name = os.getenv("MONGO_DB_NAME")
+mongo_url = os.getenv("MONGO_URL")
 
+# Create the MongoDB client using environment variables
 client = MongoClient(
-    f"mongodb+srv://{username}:{password}@dyslexia.9bswy.mongodb.net/?retryWrites=true&w=majority&appName=Dyslexia",
-    tls=True,  # Use tls instead of ssl
-    tlsAllowInvalidCertificates=True  # Ignore invalid certificates
+    f"mongodb+srv://{username}:{password}@{mongo_url}/?retryWrites=true&w=majority&appName={db_name}",
+    tls=True,  # Use TLS for encryption
+    tlsAllowInvalidCertificates=True  # Optionally allow invalid certs
 )
 
-db = client.dyscalculia
+# Access the specified database
+db = client[db_name]
 
-# Change the collection names as needed
+# Define the collections you will be working with
 user_registration_collection = db["UserRegistrations"]
 user_profile_collection = db["UserProfiles"]
 quiz_collection = db["Quizzes"]
 questions_collection = db["QuizQuestions"]
 predicted_values_collection = db["PredictedResults"]
-
-# Now, your collections have the updated names
-
-
